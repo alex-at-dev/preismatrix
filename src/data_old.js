64f55f70d29a273c0d8c5be2ab980c9c096ebc50
Alex <alex.zeiss@gmx.net>
2023-10-17 13:32:31 +0200

    Initial commit

---
+++ src/data_old.js
@@ -0,0 +1,59 @@
+window.PM__FEATURE_DATA2 = [
+  {
+    name: "Bewertungsstatus",
+    description:
+      "Erhalten Sie einmal im Monat eine Übersicht Ihrer aktuellen Bewertungen inkl. Veränderungen, die sich bei Ihnen und beim Wettbewerb ergeben haben.",
+    tiers: [1, 1, 1, 1],
+  },
+  {
+    name: "Bewertungsalarm",
+    description:
+      "Reagieren Sie unmittelbar auf Rezensionen durch eine Benachrichtigung per Mail. Keine manuellen Recherche auf verschiedenen Portalen mehr nötig.",
+    tiers: [1, 1, 1, 1],
+  },
+  {
+    name: "Bewertungskarten für Kunden vor Ort",
+    description:
+      "Einfache und gezielte Generierung positiver Bewertungen von Kunden vor Ort. Wir kümmern uns um die Digitalisierung und Veröffentlichung Ihrer Kundenstimmen.",
+    tiers: [1, 1, 1, 1],
+  },
+  {
+    name: "Empfehlungskarten mit QR-Code",
+    description:
+      "Schaffen Sie Vertrauen mit Ihren Bewertungen auf Ihrer Webseite und motivieren Sie Interessenten, häufiger mit Ihnen in Kontakt zu treten.",
+    tiers: ["optional", "optional", "optional", 1],
+  },
+  {
+    name: "TOP Anbieter Zertifikat",
+    description: "",
+    tiers: [1, 1, 1, 1],
+  },
+  {
+    name: "Online Bewertungssiegel für Ihre Webseite",
+    description:
+      "Schaffen Sie Vertrauen mit Ihren Bewertungen auf Ihrer Webseite und motivieren Sie Interessenten, häufiger mit Ihnen in Kontakt zu treten.",
+    tiers: [0, 0, 1, 1],
+  },
+  {
+    name: "Bewertungssiegel für Print & Werbung",
+    description: "",
+    tiers: [0, 0, 1, 1],
+  },
+  {
+    name: "Siegelverteilung über Eintragsservice",
+    description:
+      "Steigern Sie durch einheitliche Firmendaten Ihre Auffindbarkeit bei Google und den wichtigsten deutschen Branchenportalen inkl. reichweitenstarke Ausspielung Ihres Siegels auf 16 Portalen.",
+    tiers: [0, 0, 0, 1],
+  },
+  {
+    name: "Anzeige",
+    description: "",
+    tiers: [0, 0, 1, 1],
+  },
+  {
+    name: "Werbefläche",
+    description:
+      "Exklusive große Werbefläche in Ihrer Branche, in der einzig und allein nur Sie werben für maximale Aufmerksamkeit auf unserem Portal.",
+    tiers: ["optional", "optional", "optional", "optional"],
+  },
+];
