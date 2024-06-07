990886e953dde17ce49df12534bac79770fe07f6
Alex <alex.zeiss@gmx.net>
2024-06-07 12:27:52 +0200

    Use current data from wordpress

--- src/data.js
+++ src/data.js
@@ -6,30 +6,30 @@ window.PM__FEATURE_DATA = [
         name: 'Kostenloses Basis-Firmenprofil',
         description:
           'Ihr Unternehmen auf Deutschlands größtem Bewertungsportal mit einer Reichweite von im Schnitt über 3,5 Mio. Aufrufen pro Monat.  ',
-        tiers: [1, 1, 1, 1],
+        tiers: [1, 1, 1],
       },
       {
         name: 'Top Auffindbarkeit über Suchmaschinen',
         description:
           'Werden Sie über unser Verzeichnis in deutschen Suchmaschinenergebnissen bei Google & Bing in vielen Ihrer Branchen-Orts-Kombination ganz vorne gefunden.  ',
-        tiers: [1, 1, 1, 1],
+        tiers: [1, 1, 1],
       },
       {
         name: 'unbegrenzt Bewertungen zusammenfassen',
         description: '',
-        tiers: [1, 1, 1, 1],
+        tiers: [1, 1, 1],
       },
       {
-        name: 'Verbreitung in weitere Branchenverzeichnisse',
+        name: 'Ihr Profil in weiteren Verzeichnissen',
         description:
-          'Verlinkung aus weiteren reichweitenstarken Verzeichnissen & Partnern wie 11880.com und passenden 11880-Fachportalen.  ',
-        tiers: [1, 1, 1, 1],
+          'Verlinkung aus weiteren reichweitenstarken Verzeichnissen & Partnerportalen wie 11880.com und passenden 11880-Fachportalen.  ',
+        tiers: [1, 1, 1],
       },
       {
         name: 'Verifiziertes Firmenprofil',
         description:
           'Schaffen Sie mehr Aufmerksamkeit und Vertrauen, durch ein verifiziertes Profil.',
-        tiers: [0, 1, 1, 1],
+        tiers: [0, 1, 1],
       },
     ],
   },
@@ -37,28 +37,22 @@ window.PM__FEATURE_DATA = [
     name: 'Alles im Blick haben',
     items: [
       {
-        name: 'Bewertungsstatus',
+        name: 'Stimmungsanalyse',
         description:
-          'Erhalten Sie einmal im Monat eine Übersicht Ihrer aktuellen Bewertungen inkl. Veränderungen, die sich bei Ihnen und beim Wettbewerb ergeben haben. ',
-        tiers: [0, 1, 1, 1],
+          'Unter Einsatz künstlicher Intelligenz werden bei der Verarbeitung der Bewertungen häufig gefundenen Begriffe in ihrem jeweiligen Sinnzusammenhang als positiv (grün), neutral (grau) oder negativ (rot) dargestellt.',
+        tiers: [1, 1, 1],
+      },
+      {
+        name: 'Branchenvergleich',
+        description:
+          'Erhalten Sie einen Branchenvergleich, wie Ihr Profil im Vergleich zum Branchenschnitt bewertet ist.',
+        tiers: [1, 1, 1],
       },
       {
         name: 'Bewertungsalarm',
         description:
           'Reagieren Sie unmittelbar auf Rezensionen durch eine Benachrichtigung per Mail. Keine manuellen Recherchen auf verschiedenen Portal mehr nötig.',
-        tiers: [0, 1, 1, 1],
-      },
-      {
-        name: 'Schlagwortwolke',
-        description:
-          'Unter Einsatz künstlicher Intelligenz werden bei der Verarbeitung der Bewertungen häufigst gefundenen Begriffe in ihrem jeweiligen Sinnzusammenhang als positiv (grün), neutral (grau) oder negativ (rot) dargestellt.',
-        tiers: [1, 1, 1, 1],
-      },
-      {
-        name: 'Statistiken & Branchenvergleich',
-        description:
-          'Unter Einsatz künstlicher Intelligenz werden bei der Verarbeitung der Bewertungen häufigst gefundenen Begriffe in ihrem jeweiligen Sinnzusammenhang als positiv (grün), neutral (grau) oder negativ (rot) dargestellt.',
-        tiers: [1, 1, 1, 1],
+        tiers: [0, 1, 1],
       },
     ],
   },
@@ -66,27 +60,27 @@ window.PM__FEATURE_DATA = [
     name: 'Bewertungen managen',
     items: [
       {
-        name: 'Weitere Bewertungsquellen hinzufügen',
+        name: 'Selbst Bewertungsquellen hinzufügen',
         description: 'Fügen Sie im Kundenbereich weitere Portale zu Ihrer Zusammenfassung hinzu.',
-        tiers: [0, 1, 1, 1],
+        tiers: [0, 1, 1],
+      },
+      {
+        name: 'QR-Code für Bewertungsgenerierung',
+        description:
+          'Sammeln Sie mit einem QR-Code Bewertungen überall dort, wo Sie ein Firmenprofil haben. Der QR-Code steht zum Download im Kundenbereich bereit.',
+        tiers: [0, 1, 1],
       },
       {
         name: 'Bewertungskarten für Kunden vor Ort',
         description:
           'Einfache und gezielte Generierung positiver Bewertungen von Kunden vor Ort. Wir kümmern uns um die Digitalisierung Ihrer Kundenstimmen.',
-        tiers: ['optional', 1, 1, 1],
+        tiers: [0, 'optional', 1],
       },
       {
         name: 'Sammelbox',
         description:
           'Nutzen Sie unsere Sammelbox als Aufsteller für den Einwurf von ausgefüllten Bewertungskarten bei Ihnen vor Ort im Geschäft.',
-        tiers: [0, 'optional', 'optional', 1],
-      },
-      {
-        name: 'Empfehlungskarten mit QR-Code',
-        description:
-          'Sammeln Sie mit einer Karte Bewertungen überall dort, wo Sie ein Firmenprofil haben und legen Sie die Karten unkompliziert Ihrem Geschäftsverkehr bei. Visitenkartenformat inkl. Nutzungsreport.',
-        tiers: ['optional', 'optional', 'optional', 1],
+        tiers: [0, 'optional', 1],
       },
     ],
   },
@@ -96,31 +90,31 @@ window.PM__FEATURE_DATA = [
       {
         name: 'Online Bewertungssiegel für Ihre Webseite',
         description:
-          'Binden Sie Siegel-Widget auf Ihrer Webseite ein. Das Siegel fasst alle Ihre Bewertungen aus dem Netz zusammen. Zudem transportiert es Ihre Bewertungssterne in die Google-Suchergebnisse Ihrer Unterseiten.',
-        tiers: [0, 0, 1, 1],
+          'Binden Sie das Bewertungssiegel auf Ihrer Webseite ein. Das Siegel fasst Ihre Bewertungen aus dem Netz zusammen. Zudem transportiert es Ihre Bewertungssterne aus Ihrer Unterseite in die Google-Suchergebnisse.',
+        tiers: [0, 1, 1],
       },
       {
         name: 'Bewertungssiegel für Print & Werbung',
         description:
           'Für Print-Unterlagen wie Flyer und Briefbögen, erhalten Sie ein zusätzliches individuelles Siegel als PDF-Download.',
-        tiers: [0, 0, 1, 1],
+        tiers: [0, 1, 1],
       },
       {
         name: 'Siegelverteilung über Eintragsservice',
         description:
           'Steigern Sie durch einheitliche Firmendaten Ihre Auffindbarkeit bei Google und den wichtigsten Branchenportalen inkl.  Ausspielung Ihres Siegels auf bis zu 16 Portalen.',
-        tiers: [0, 0, 1, 1],
+        tiers: [0, 0, 1],
       },
       {
-        name: 'Aufkleber "Top bewertet" für die Außenwerbung',
+        name: 'Aufkleber für die Außenwerbung',
         description: 'Nutzen Sie die Aufkleber für die Außenwerbung auf Ihren Schaufenstern.',
-        tiers: [0, 0, 1, 1],
+        tiers: [0, 0, 1],
       },
       {
         name: 'TOP Anbieter Zertifikat',
         description:
-          'Ab einem Bewertungsschnitt von 4,5 Sternen können Sie einmal im Monat das digitale TOP-Anbieter Zertifikate anfordern und damit werben.',
-        tiers: [0, 1, 1, 1],
+          'Ab einem Bewertungsschnitt von 4,5 Sternen und 50 Bewertungen können Sie einmal im Monat das digitale TOP-Anbieter Zertifikate anfordern und damit werben.',
+        tiers: [0, 0, 'optional'],
       },
     ],
   },
@@ -131,13 +125,13 @@ window.PM__FEATURE_DATA = [
         name: 'Anzeige',
         description:
           'Ausspielung Ihres Firmenprofils auf unserem Portal vor allen anderen Mitbewerbern Ihrer Branche. ',
-        tiers: [0, 0, 1, 1],
+        tiers: [0, 0, 1],
       },
       {
         name: 'Werbefläche',
         description:
           'Exklusive große Werbefläche in Ihrer Branche, in der einzig und allein nur Sie werben für maximale Aufmerksamkeit auf unserem Portal.',
-        tiers: [0, 'optional', 'optional', 'optional'],
+        tiers: [0, 0, 'optional'],
       },
     ],
   },
@@ -145,21 +139,26 @@ window.PM__FEATURE_DATA = [
     name: 'Service',
     items: [
       {
-        name: 'Kostenloser E-Mail Support',
+        name: 'E-Mail Support',
         description:
-          'Wir sind für Sie da. Unser E-Mail Support steht Ihnen jederzeit zur Seite und hilft bei Fragen und Problemen gerne weiter.',
-        tiers: [0, 1, 1, 1],
+          'Wir sind für Sie da. Unser E-Mail Support steht Ihnen zu Geschäftszeiten zur Seite und hilft bei Fragen und Problemen gerne weiter.',
+        tiers: [1, 1, 1],
+      },
+      {
+        name: 'Prio E-Mail Support',
+        description: 'Vorrangige Bearbeitung Ihrer Anfragen per E-Mail.',
+        tiers: [0, 1, 1],
+      },
+      {
+        name: 'Persönlicher Kundenservice',
+        description: 'Wir sind telefonisch für Sie da und zu den normalen Bürozeiten erreichbar.',
+        tiers: [0, 1, 1],
       },
       {
         name: 'Tipps & Tricks zum Reputationsmanagement',
         description:
           'Wir bieten Ihnen regelmäßige Tipps und Tricks, wie Sie Ihre Reputation im Internet verbessern und somit Ihren Umsatz verbessern können.',
-        tiers: [0, 1, 1, 1],
-      },
-      {
-        name: 'Persönlicher Kundenservice',
-        description: 'Wir sind telefonisch für Sie da und zu den normalen Bürozeiten erreichbar.',
-        tiers: [0, 1, 1, 1],
+        tiers: [0, 0, 1],
       },
     ],
   },
